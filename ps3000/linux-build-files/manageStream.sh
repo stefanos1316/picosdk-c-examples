ae19a88a1acef1ca754ec3f37c7cc27e41a597ba
stefanos1316 <stefanos1316@gmail.com>
2019-09-27 09:11:38 +0300

    Automating script further

--- ps3000/linux-build-files/manageStream.sh
+++ ps3000/linux-build-files/manageStream.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [ $1 == "start"  ]; then
+	
 	echo -ne "\n" | ./ps3000Con S
 fi
 
 if [ $1 == "stop" ]; then
-	echo -ne "\n" > /proc/$2/fd/0
+	pid=$(ps -aux | grep ps3000 | head -1 | awk '{print $2}')
+	echo -ne "\n" > /proc/$pid/fd/0
 fi
 
 exit
--- ps3000/linux-build-files/stopStream.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -ne "\n" > /proc/$1/fd/0
